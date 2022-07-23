154509d88990d0a4a7e94c8c3cf7b999a35a7afc
Daniel Li <dan.li2010@hotmail.com>
2022-07-23 14:55:45 -0400

    initial commit

---
+++ api/CrimeDataHandler.py
@@ -0,0 +1,31 @@
+from flask_restful import Api, Resource, reqparse
+
+class CrimeDataHandler(Resource):
+    def get(self):
+        return {
+            'resultStatus': 'SUCCESS',
+            'message': 'This should be a success!'
+        }
+
+    def post(self):
+        print(self)
+        parser = reqparse.RequestParser()
+        parser.add_argument('type', type=str)
+        parser.add_argument('message', type=str)
+
+        args = parser.parse_args()
+        print(args)
+
+        request_type = args['type']
+        request_json = args['message']
+
+        ret_status = request_type
+        ret_msg = request_json
+
+        if ret_msg:
+            message = f'successful message: {ret_msg}'
+        else:
+            message = 'failed msg'
+
+        final_ret = {"status": "Success", "message": message}
+        return final_ret
---
+++ api/data.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+from pip import main
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
+CRIME_DATA_URL = 'https://rupdadmin.rice.edu/crimelog/unskinned/'
+
+class CrimeDataParser:
+
+    def __init__(self):
+        pass
+
+    def get_crime_data_html(self, url):
+        crime_log_request = requests.get(url)
+        return BeautifulSoup(crime_log_request.text, 'lxml')
+    
+    def create_df(self, crime_html_data):
+        case_number = [td.text for td in crime_html_data.find_all('td',{'class':'case_number'})]
+        crime_type = [td.text for td in crime_html_data.find_all('td',{'class':'classification'})]
+        location = [td.text for td in crime_html_data.find_all('td',{'class':'location'})]
+        time_reported = [td.text for td in crime_html_data.find_all('td',{'class':'datetime_reported'})]
+
+        data = {
+            'case_number': case_number,
+            'crime_type': crime_type,
+            'location': location,
+            'time_reported': time_reported
+        }
+
+        df = pd.DataFrame(data = data, columns = data.keys())
+
+        time_format_replacements = {
+            'a.m.':'AM',
+            'p.m.':'PM',
+            'noon':'12:00 PM',
+            'midnight':'12:00 AM'
+        }
+
+        df['time_reported'] = df['time_reported'].replace(time_format_replacements, regex=True)
+
+        df['time_reported'] = pd.to_datetime(df['time_reported'], format='%m/%d/%Y %I:%M %p')
+
+        return df
+    
+if __name__ == '__main__':
+    cdp = CrimeDataParser()
+    data_html = cdp.get_crime_data_html(CRIME_DATA_URL)
+    print(cdp.create_df(data_html))
